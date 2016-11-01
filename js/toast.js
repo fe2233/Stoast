98017f7229587c19e472093a67a81bedcda1cf75
fe2233 <liujia10283008@163.com>
2016-11-02 00:38:51 +0800

    Update toast.js

--- js/toast.js
+++ js/toast.js
@@ -1,63 +1,69 @@
-﻿var setting={
-	 height:45,
-	 fontSize:14,
-	 color:"#fff",
-	 backgroundColor:"rgba(0,0,0,0.8)",
-	 time:3000
- };
-function init(options){
-	setting=$.extend({},setting,options);
-}
-
-var intervalCounter = [];
-var clientHeight = $(window).height();
-var h, f, c, b, t;
-  
- var drawToast = function(text) {
-	  h=setting.height;
-	  f=setting.fontSize;
-	  c = setting.color;
-	  b = setting.backgroundColor;
-	  t = setting.time;
-  
-	var toast = $(".toast");
+function toast(options) {
+	var intervalCounter = [], clientHeight = 0;
+	var h, f, c, b, t;
+	var setting={
+		 height:45,
+		 fontSize:14,
+		 color:"#fff",
+		 backgroundColor:"rgba(0,0,0,0.8)",
+		 time:3000
+	 };
+	 
+	 function init(){
+		setting=$.extend({},setting,options);
+		clientHeight = $(window).height();
 		
-	var bottomPos = null;
-	var index = null;
-	var bottom = clientHeight*5/100;
-	
-	if(toast == null) {
-		bottomPos = bottom;
-		index = 0;
-	} else {
-		index = toast.length;
-		bottomPos = toast.length*55+bottom;
+		h=setting.height;
+		f=setting.fontSize;
+		c = setting.color;
+		b = setting.backgroundColor;
+		t = setting.time;
 	}
 	
-	var toastHTML = '<div class="toast" index="'+index+'">'+text+'</div>';
-	$("body").append(toastHTML);
+	function drawToast(text) {
+		var toast = $(".toast");
+			
+		var bottomPos = null;
+		var index = null;
+		var bottom = clientHeight*5/100;
+		
+		if(toast == null) {
+			bottomPos = bottom;
+			index = 0;
+		} else {
+			index = toast.length;
+			bottomPos = toast.length*55+bottom;
+		}
+		
+		var toastHTML = '<div class="toast" index="'+index+'">'+text+'</div>';
+		$("body").append(toastHTML);
+		
+		var newestToast = $(".toast[index='"+index+"']");
+		newestToast.css({"height":h+"px","line-height":h+"px","font-size":f+"px","color":c,"background-color":b,
+			"position":"fixed","width":"40%","border-radius":"5px","text-align":"center","z-index":"1200","display":"none",
+			"bottom":bottomPos+"px","left":"30%","transition":"all 0.3s ease","opacity":0,"box-shadow":"0px 0px 10px 0px rgba(0, 0, 0, 0.2)"
+		});
 	
-	var newestToast = $(".toast[index='"+index+"']");
-	newestToast.css({"height":h+"px","line-height":h+"px","font-size":f+"px","color":c,"background-color":b,
-		"position":"fixed","width":"40%","border-radius":"5px","text-align":"center","z-index":"1200","display":"none",
-		"bottom":bottomPos+"px","left":"30%","transition":"all 0.3s ease","opacity":0,"box-shadow":"0px 0px 10px 0px rgba(0, 0, 0, 0.2)"
-	});
-
-	newestToast.html(text);
-	newestToast.show();
-	newestToast.css("opacity", 1);
-	
-	intervalCounter[index] = setInterval(function() {
-		hideToast(index);
-	}, t);
-};
+		newestToast.html(text);
+		newestToast.show();
+		newestToast.css("opacity", 1);
+		
+		intervalCounter[index] = setInterval(function() {
+			hideToast(index);
+		}, t);
+	};
  
-var hideToast = function(index) {
-	var toastView = $(".toast[index='"+index+"']");
-	toastView.css("opacity", 0);
-	setTimeout(function() {
-		toastView.hide();
-		clearInterval(intervalCounter[index]);
-		$(".toast[index='"+index+"']").remove();
-	}, 300);
-};
+	function hideToast(index) {
+		var toastView = $(".toast[index='"+index+"']");
+		toastView.css("opacity", 0);
+		setTimeout(function() {
+			toastView.hide();
+			clearInterval(intervalCounter[index]);
+			$(".toast[index='"+index+"']").remove();
+		}, 300);
+	};
+	
+	init();
+	
+	return drawToast;
+}
