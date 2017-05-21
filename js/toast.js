af049216b96ae22953dd9446f385f5ec5aa66b9a
liujia <liujia10283008@163.com>
2017-05-21 17:20:13 +0800

    change something

--- js/toast.js
+++ js/toast.js
@@ -1,69 +1,55 @@
-function toast(options) {
-	var intervalCounter = [], clientHeight = 0;
-	var h, f, c, b, t;
-	var setting={
-		 height:45,
-		 fontSize:14,
-		 color:"#fff",
-		 backgroundColor:"rgba(0,0,0,0.8)",
-		 time:3000
-	 };
-	 
-	 function init(){
-		setting=$.extend({},setting,options);
-		clientHeight = $(window).height();
-		
-		h=setting.height;
-		f=setting.fontSize;
-		c = setting.color;
-		b = setting.backgroundColor;
-		t = setting.time;
-	}
+﻿function toast() {
+	var iconDatas = ["iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAADsSURBVEhLY2AYBfQMgf///3P8+/evAIgvA/FsIF+BavYDDWMBGroaSMMBiE8VC7AZDrIFaMFnii3AZTjUgsUUWUDA8OdAH6iQbQEhw4HyGsPEcKBXBIC4ARhex4G4BsjmweU1soIFaGg/WtoFZRIZdEvIMhxkCCjXIVsATV6gFGACs4Rsw0EGgIIH3QJYJgHSARQZDrWAB+jawzgs+Q2UO49D7jnRSRGoEFRILcdmEMWGI0cm0JJ2QpYA1RDvcmzJEWhABhD/pqrL0S0CWuABKgnRki9lLseS7g2AlqwHWQSKH4oKLrILpRGhEQCw2LiRUIa4lwAAAABJRU5ErkJggg==","iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGwSURBVEhLtZa9SgNBEMc9sUxxRcoUKSzSWIhXpFMhhYWFhaBg4yPYiWCXZxBLERsLRS3EQkEfwCKdjWJAwSKCgoKCcudv4O5YLrt7EzgXhiU3/4+b2ckmwVjJSpKkQ6wAi4gwhT+z3wRBcEz0yjSseUTrcRyfsHsXmD0AmbHOC9Ii8VImnuXBPglHpQ5wwSVM7sNnTG7Za4JwDdCjxyAiH3nyA2mtaTJufiDZ5dCaqlItILh1NHatfN5skvjx9Z38m69CgzuXmZgVrPIGE763Jx9qKsRozWYw6xOHdER+nn2KkO+Bb+UV5CBN6WC6QtBgbRVozrahAbmm6HtUsgtPC19tFdxXZYBOfkbmFJ1VaHA1VAHjd0pp70oTZzvR+EVrx2Ygfdsq6eu55BHYR8hlcki+n+kERUFG8BrA0BwjeAv2M8WLQBtcy+SD6fNsmnB3AlBLrgTtVW1c2QN4bVWLATaIS60J2Du5y1TiJgjSBvFVZgTmwCU+dAZFoPxGEEs8nyHC9Bwe2GvEJv2WXZb0vjdyFT4Cxk3e/kIqlOGoVLwwPevpYHT+00T+hWwXDf4AJAOUqWcDhbwAAAAASUVORK5CYII=", "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGYSURBVEhL5ZSvTsNQFMbXZGICMYGYmJhAQIJAICYQPAACiSDB8AiICQQJT4CqQEwgJvYASAQCiZiYmJhAIBATCARJy+9rTsldd8sKu1M0+dLb057v6/lbq/2rK0mS/TRNj9cWNAKPYIJII7gIxCcQ51cvqID+GIEX8ASG4B1bK5gIZFeQfoJdEXOfgX4QAQg7kH2A65yQ87lyxb27sggkAzAuFhbbg1K2kgCkB1bVwyIR9m2L7PRPIhDUIXgGtyKw575yz3lTNs6X4JXnjV+LKM/m3MydnTbtOKIjtz6VhCBq4vSm3ncdrD2lk0VgUXSVKjVDJXJzijW1RQdsU7F77He8u68koNZTz8Oz5yGa6J3H3lZ0xYgXBK2QymlWWA+RWnYhskLBv2vmE+hBMCtbA7KX5drWyRT/2JsqZ2IvfB9Y4bWDNMFbJRFmC9E74SoS0CqulwjkC0+5bpcV1CZ8NMej4pjy0U+doDQsGyo1hzVJttIjhQ7GnBtRFN1UarUlH8F3xict+HY07rEzoUGPlWcjRFRr4/gChZgc3ZL2d8oAAAAASUVORK5CYII=", "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAHOSURBVEhLrZa/SgNBEMZzh0WKCClSCKaIYOED+AAKeQQLG8HWztLCImBrYadgIdY+gIKNYkBFSwu7CAoqCgkkoGBI/E28PdbLZmeDLgzZzcx83/zZ2SSXC1j9fr+I1Hq93g2yxH4iwM1vkoBWAdxCmpzTxfkN2RcyZNaHFIkSo10+8kgxkXIURV5HGxTmFuc75B2RfQkpxHG8aAgaAFa0tAHqYFfQ7Iwe2yhODk8+J4C7yAoRTWI3w/4klGRgR4lO7Rpn9+gvMyWp+uxFh8+H+ARlgN1nJuJuQAYvNkEnwGFck18Er4q3egEc/oO+mhLdKgRyhdNFiacC0rlOCbhNVz4H9FnAYgDBvU3QIioZlJFLJtsoHYRDfiZoUyIxqCtRpVlANq0EU4dApjrtgezPFad5S19Wgjkc0hNVnuF4HjVA6C7QrSIbylB+oZe3aHgBsqlNqKYH48jXyJKMuAbiyVJ8KzaB3eRc0pg9VwQ4niFryI68qiOi3AbjwdsfnAtk0bCjTLJKr6mrD9g8iq/S/B81hguOMlQTnVyG40wAcjnmgsCNESDrjme7wfftP4P7SP4N3CJZdvzoNyGq2c/HWOXJGsvVg+RA/k2MC/wN6I2YA2Pt8GkAAAAASUVORK5CYII="];
 	
-	function drawToast(text) {
-		var toast = $(".toast");
-			
-		var bottomPos = null;
-		var index = null;
-		var bottom = clientHeight*5/100;
+	var title, message, type, time, toast, typeText, toastHTML;
+	
+	function init() {
+		title = $("#title").val();
+		message = $("#message").val();
+		type = parseInt($("input:radio:checked").val());
+		time = parseInt($("#timeout").val());
 		
-		if(toast == null) {
-			bottomPos = bottom;
-			index = 0;
-		} else {
-			index = toast.length;
-			bottomPos = toast.length*55+bottom;
+		if(!(title != "") && (message != "")) {
+			alert("less parms");
+			return false;
 		}
 		
-		var toastHTML = '<div class="toast" index="'+index+'">'+text+'</div>';
+		return true;
+	};
+	
+	function drawToast() {
+		if(!init())
+			return;
+		
+		switch(type) {
+			case 0:typeText = "success";
+				break;
+			case 1:typeText = "info";
+				break;
+			case 2:typeText = "warning";
+				break;
+			case 3:typeText = "error";
+				break;
+		}
+		
+		toastHTML = '<div class="toast toast-'+typeText+'">'+
+							'<p class="toast-title">'+title+'</p>'+
+							'<p class="toast-message">'+message+'</p>'+
+						'</div>';
 		$("body").append(toastHTML);
 		
-		var newestToast = $(".toast[index='"+index+"']");
-		newestToast.css({"height":h+"px","line-height":h+"px","font-size":f+"px","color":c,"background-color":b,
-			"position":"fixed","width":"40%","border-radius":"5px","text-align":"center","z-index":"1200","display":"none",
-			"bottom":bottomPos+"px","left":"30%","transition":"all 0.3s ease","opacity":0,"box-shadow":"0px 0px 10px 0px rgba(0, 0, 0, 0.2)"
-		});
-	
-		newestToast.html(text);
-		newestToast.show();
-		newestToast.css("opacity", 1);
+		toast = $(".toast");
+		toast.css("background-image", "url(data:image/png;base64,"+iconDatas[type]+")");
 		
-		intervalCounter[index] = setInterval(function() {
-			hideToast(index);
-		}, t);
+		setTimeout(function() {
+			hideToast();
+		}, time);
 	};
  
-	function hideToast(index) {
-		var toastView = $(".toast[index='"+index+"']");
-		toastView.css("opacity", 0);
-		setTimeout(function() {
-			toastView.hide();
-			clearInterval(intervalCounter[index]);
-			$(".toast[index='"+index+"']").remove();
-		}, 300);
+	function hideToast() {
+		toast.remove();
 	};
-	
-	init();
 	
 	return drawToast;
 }
+ 
